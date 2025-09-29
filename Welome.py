33b08719d6dba2369da46e982a625ce85efe67e8
alphonsasummer <alphonsasummer@gmail.com>
2025-09-28 18:58:05 -0700

    second commit

--- Welome.py
+++ Welome.py
@@ -1,3 +1,4 @@
-print("Wlecome to Python Programming!")
+print("Welcome to Python Programming!")
 print("This is a simple Python script.")
 print("Enjoy coding!")  
+print("Merged code snippnet from Git to GitHub")
